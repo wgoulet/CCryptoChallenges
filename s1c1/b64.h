edcc51ea70e160feb68878f785073370864ce449
wgoulet <wgoulet@gmail.com>
2015-04-19 16:42:02 +1000

    Got basic base64 encoder working but padding not right

--- s1c1/b64.h
+++ s1c1/b64.h
@@ -18,7 +18,42 @@
 16 Q            33 h            50 y
 */
 
-char *b64dict[65] = 
+struct b64alpha
+{
+	int value;
+	char encoding;
+
+};
+
+struct b64chunk
+{
+	unsigned int f1:1;
+	unsigned int f2:1;
+	unsigned int f3:1;
+	unsigned int f4:1;
+	unsigned int f5:1;
+	unsigned int f6:1;
+	unsigned int f7:1;
+	unsigned int f8:1;
+	unsigned int f9:1;
+	unsigned int f10:1;
+	unsigned int f11:1;
+	unsigned int f12:1;
+	unsigned int f13:1;
+	unsigned int f14:1;
+	unsigned int f15:1;
+	unsigned int f16:1;
+	unsigned int f17:1;
+	unsigned int f18:1;
+	unsigned int f19:1;
+	unsigned int f20:1;
+	unsigned int f21:1;
+	unsigned int f22:1;
+	unsigned int f23:1;
+	unsigned int f24:1;
+};
+
+char *b64dict[64] = 
 {"0,A","1,B","2,C","3,D","4,E","5,F","6,G","7,H",
 "8,I","9,J","10,K","11,L","12,M","13,N","14,O",
 "15,P","16,Q","17,R","18,S","19,T","20,U","21,V",
@@ -27,5 +62,4 @@ char *b64dict[65] =
 "36,k","37,l","38,m","39,n","40,o","41,p","42,q",
 "43,r","44,s","45,t","46,u","47,v","48,w","49,x",
 "50,y","51,z","52,0","53,1","54,2","55,3","56,4",
-"57,5","58,6","59,7","60,8","61,9","62,+","63,/",
-"(pad),="};
+"57,5","58,6","59,7","60,8","61,9","62,+","63,/"};
--- s1c1/program.c
+++ s1c1/program.c
@@ -13,18 +13,138 @@
 #include <netdb.h>
 #include "b64.h"
 
-const size_t MAXINT=65536;
+const size_t MAXSTR=65536;
 
-void main(int argc,char argv[])
+void main(int argc,char* argv[])
 {
-	size_t alphalen = 65;
-	char* disp = malloc(MAXINT);
+	size_t alphalen = 64;
+	char* disp = malloc(MAXSTR);
+	struct b64alpha b64lookup[64];
+	if(argc != 2)
+	{
+		puts("No filename specified; exiting");
+		return;
+	}
 	for(int i = 0; i < alphalen; i++)
         {
 	        char* e = b64dict[i];
-		snprintf(disp,255,"%s",e);
-		puts(disp);
+		char* enc = malloc(255);
+		errno = 0;
+		// strtol stops parsing number
+		// at first non-numeric char
+		// so enc will point to remaining
+		// string
+		int v = strtol(b64dict[i],&enc,10);
+		if(errno == 0)
+		{
+			b64lookup[i].value = v;
+			// skip past the comma in our  dictionary
+			enc++;
+			// store the char in the encoding
+			b64lookup[i].encoding = *enc;
+			//memcpy(&b64lookup[i].encoding,enc,strnlen(enc,MAXSTR));
+			////free(enc);
+		}
 		
+		//snprintf(disp,255,"%d -> %c",b64lookup[i].value,b64lookup[i].encoding);
         }
 
+	FILE* fp = fopen(argv[1],"r");
+	int charct = 0;
+	for(;;)
+	{
+		if(feof(fp))
+		{
+			break;
+		}
+		struct b64chunk* chunk = malloc(sizeof(struct b64chunk));
+		bzero(chunk,sizeof(struct b64chunk));
+		int numb = fread(chunk,1,3,fp);
+                if(numb < 3)
+                {
+			//snprintf(disp,255,"Bytes padding: %d",numb);
+			for(int j = 0; j < numb; j++)
+			{
+				putchar('=');
+				charct++;
+			}
+			break;
+		}
+		// construct 6 bit values by adding
+		// each field value to an int, shifting left
+		// after each add
+                
+		int char1 = 0;
+		char1 += chunk->f8;
+		char1 = char1 << 1;
+		char1 += chunk->f7;
+		char1 = char1 << 1;
+		char1 += chunk->f6;
+		char1 = char1 << 1;
+		char1 += chunk->f5;
+		char1 = char1 << 1;
+		char1 += chunk->f4;
+		char1 = char1 << 1;
+		char1 += chunk->f3;
+		snprintf(disp,255,"%d",b64lookup[char1].encoding);
+		putchar(b64lookup[char1].encoding);
+		charct++;
+
+		int char2 = 0;
+		char2 += chunk->f2;
+		char2 = char2 << 1;
+		char2 += chunk->f1;
+		char2 = char2 << 1;
+		char2 += chunk->f16;
+		char2 = char2 << 1;
+		char2 += chunk->f15;
+		char2 = char2 << 1;
+		char2 += chunk->f14;
+		char2 = char2 << 1;
+		char2 += chunk->f13;
+		snprintf(disp,255,"%d",b64lookup[char2].encoding);
+		putchar(b64lookup[char2].encoding);
+		charct++;
+
+		int char3 = 0;
+		char3 += chunk->f12;
+		char3 = char3 << 1;
+		char3 += chunk->f11;
+		char3 = char3 << 1;
+		char3 += chunk->f10;
+		char3 = char3 << 1;
+		char3 += chunk->f9;
+		char3 = char3 << 1;
+		char3 += chunk->f24;
+		char3 = char3 << 1;
+		char3 += chunk->f23;
+		snprintf(disp,255,"%d",b64lookup[char3].encoding);
+		putchar(b64lookup[char3].encoding);
+		charct++;
+
+		int char4 = 0;
+		char4 += chunk->f22;
+		char4 = char4 << 1;
+		char4 += chunk->f21;
+		char4 = char4 << 1;
+		char4 += chunk->f20;
+		char4 = char4 << 1;
+		char4 += chunk->f19;
+		char4 = char4 << 1;
+		char4 += chunk->f18;
+		char4 = char4 << 1;
+		char4 += chunk->f17;
+		snprintf(disp,255,"%d",b64lookup[char4].encoding);
+		putchar(b64lookup[char4].encoding);
+		charct++;
+		// linewrap output at 76 chars
+		// to match gnu base64
+		if(charct % 76 == 0)
+		{
+			puts("");
+		}
+		free(chunk);
+	}	
+	free(disp);
+	puts("");
 }
